9ae4d607b583e8a827c4fda3bbac27fa201c9f2c
Adam England <adam.n.england@gmail.com>
2013-12-04 22:59:21 -0600

    Adding additonal metrics

--- jobs/apiMetrics.rb
+++ jobs/apiMetrics.rb
@@ -1,6 +1,10 @@
 require 'httparty'
 
-SCHEDULER.every '5m' do
+SCHEDULER.every '10s' do
   response = HTTParty.get('http://api.knoda.com/api/metrics')
   send_event('users', { current:  response['userMetrics']['current'], last: response['userMetrics']['lag7']})
+  c = (response['commentRatio']['current']*100).to_i / 100.0
+  ch = (response['challengeRatio']['current']*100).to_i / 100.0
+  send_event('comments', {current: c})
+  send_event('challenges', {current: ch})
 end
