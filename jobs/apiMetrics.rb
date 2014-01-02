116e28d73d75be71e242b825529ff570b23e0358
Adam England <adam.n.england@gmail.com>
2014-01-02 10:02:20 -0600

    Adding category data

--- jobs/apiMetrics.rb
+++ jobs/apiMetrics.rb
@@ -7,4 +7,19 @@ SCHEDULER.every '5m' do
   ch = (response['challengeRatio']['current']*100).to_i / 100.0
   send_event('comments', {current: c})
   send_event('challenges', {current: ch})
+  predictionCats(response)
+end
+
+def predictionCats(response)
+  pc = []
+  total = 0
+  response['categories'].each do |c|
+  	total = total + c['count']
+  end  
+  pc << {label: 'Total', value: total}
+  response['categories'].each do |c|
+  	percentageOfTotal = ((c['count'].to_f / total.to_f) * 100.0).floor
+  	pc << {label: c['name'], value: "#{percentageOfTotal}%"}
+  end
+  send_event('prediction-categories', {items: pc})
 end
